36125f1b910cecb6f2eade50f5b3c7791fd03f23
Artyom Pugachev <artempugachev1@gmail.com>
2020-06-24 23:09:14 +0300

    add posts for cycle

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,15 @@ from app import app
 def index():
     user = {'username': 'Miguel'}
     
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
