b8e6117e84f6ea5d64af7bd8450f133c2eeaca38
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-03 22:32:56 +0300

    add set and check password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
@@ -9,7 +10,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
