74ee7f5910c3f8e1c041cf2c69168365e7b94639
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-14 14:41:01 +0300

    fix type

--- app/routes.py
+++ app/routes.py
@@ -104,7 +104,7 @@ def edit_profile():
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
-        db.session.commmit()
+        db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
     
