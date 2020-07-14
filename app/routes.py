2bb4250739648fcbe1144a1a62e8bbfaafdf5bb9
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-14 12:05:06 +0300

    add before request to track last seen time

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
@@ -86,3 +87,10 @@ def user(username):
     ]
 
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
