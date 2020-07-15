d0c155fdc192290177bf159834e26165b8ab79b8
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-15 12:04:24 +0300

    add followed relationship

--- app/models.py
+++ app/models.py
@@ -15,6 +15,13 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
