5259aacadeaf353e8360189ebeaa5a6345319716
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-15 12:10:00 +0300

    migrations

--- app/models.py
+++ app/models.py
@@ -6,6 +6,14 @@ from hashlib import md5
 from app import db, login
 
 
+# followers association table
+# don't declare this table as a model.
+# this is an auxillary table that has no data other than the foreign keys
+followers = db.Table('followers', 
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+    
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -53,11 +61,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-# followers association table
-# don't declare this table as a model.
-# this is an auxillary table that has no data other than the foreign keys
-followers = db.Table('followers', 
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
