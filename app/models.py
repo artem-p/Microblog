b75923be8badce1c320842cd55179e0e45f0fa7b
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-15 17:40:57 +0300

    combine own and union posts

--- app/models.py
+++ app/models.py
@@ -57,11 +57,14 @@ class User(UserMixin, db.Model):
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
-                            
+                followers.c.follower_id == self.id)
+
+        own = Post.query.filter_by(user_id=self.id)
+        
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
