404cafaffb241ea6b1deba41928a0ad992fea584
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-15 12:47:01 +0300

    add followed posts query

--- app/models.py
+++ app/models.py
@@ -55,7 +55,13 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
-            
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+                            
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
