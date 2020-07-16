2c42e0d175628a539ed2aefe8efd53d16fabc297
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-16 12:44:51 +0300

    add follow unfollow test

--- tests.py
+++ tests.py
@@ -25,6 +25,37 @@ class UserModelCase(unittest.TestCase):
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
 
+    def test_follow_unfollow(self):
+        john = User(username='john', email='john@example.com')
+        susan = User(username='susan', email='susan@example.com')
+
+        db.session.add(john)
+        db.session.add(susan)
+        db.session.commit()
+
+        self.assertEqual(john.followed.all(), [])
+        self.assertEqual(susan.followed.all(), [])
+
+        
+        john.follow(susan)
+        db.session.commit()
+
+        self.assertTrue(john.is_following(susan))
+        self.assertEqual(john.followed.count(), 1)
+        self.assertEqual(john.followed.first().username, 'susan')
+
+        self.assertEqual(susan.followers.count(), 1)
+        self.assertEqual(susan.followers.first().username, 'john')
+
+        
+        john.unfollow(susan)
+        db.session.commit()
+
+        self.assertFalse(john.is_following(susan))
+        self.assertEqual(john.followed.count(), 0)
+        self.assertEqual(susan.followers.count(), 0)
+
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
