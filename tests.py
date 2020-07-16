622fbb224ff2a2350264f3ab57771682bb8ab25e
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-16 12:23:53 +0300

    add passwords and avatar tests

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from app import app, db
+from app.models import User
+import unittest
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -9,3 +11,20 @@ class UserModelCase(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def test_password_hashing(self):
+        user = User(username='susan')
+        user.set_password('cat')
+        self.assertFalse(user.check_password('dog'))
+        self.assertTrue(user.check_password('cat'))
+
+    def test_avatar(self):
+        user = User(usernam='john', email='john@example.com')
+
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
