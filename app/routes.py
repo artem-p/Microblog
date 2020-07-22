9dcd73cfb659fda4dcc13f0061f8ce2e2c125312
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-22 23:06:14 +0300

    pagination in user page

--- app/routes.py
+++ app/routes.py
@@ -88,14 +88,17 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
 
     empty_form = EmptyForm()
 
-    return render_template('user.html', user=user, posts=posts, form=empty_form)
+    return render_template('user.html', user=user, posts=posts.items, form=empty_form, next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
