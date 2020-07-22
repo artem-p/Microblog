c5d68a90efbcbbea1faf8cbaf0c2275d31c62ce5
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-22 22:44:08 +0300

    add pagination urls

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,10 @@ def index():
 
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
 
-    return render_template('index.html', title='Home', form=form, posts=posts.items)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -180,4 +183,7 @@ def explore():
 
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
 
-    return render_template('index.html', title='Explore', posts=posts.items)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
