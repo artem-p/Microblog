34c5dd1e10a102c3eaf46a3c72057e01c42fd89a
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-27 21:35:50 +0300

    init flask-babel

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 import os
 from logging.handlers import RotatingFileHandler
 
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 import logging
