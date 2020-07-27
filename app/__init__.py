12c850405ac6390ed677c9a72848e2dcc19db9eb
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-27 22:28:11 +0300

    don't really need i18n right now

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
 import os
 from logging.handlers import RotatingFileHandler
-from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,7 +16,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-babel = Babel(app)
 
 from app import routes, models, errors
 import logging
@@ -34,8 +31,3 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-
-
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
